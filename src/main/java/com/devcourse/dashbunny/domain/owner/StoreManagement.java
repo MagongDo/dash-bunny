3810aae95e111a357d4794f84323b103834c6d3b
Hojeong016 <skflsehdn@naver.com>
2024-11-16 00:12:14 +0900

    feat: 새로운 파일 구조 설정

---
+++ src/main/java/com/devcourse/dashbunny/domain/owner/StoreManagement.java
@@ -0,0 +1,13 @@
+package com.devcourse.dashbunny.domain.owner;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class StoreManagement {
+
+    @Id
+    @GeneratedValue
+    private Long id; // 임시 코드입니다.
+}
