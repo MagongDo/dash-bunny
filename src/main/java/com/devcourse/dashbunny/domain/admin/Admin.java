dfb45d8eb2b111ce4643460b776a8004799710ac
Hojeong016 <skflsehdn@naver.com>
2024-11-16 00:16:14 +0900

    feat: 새로운 파일 구조 설정

---
+++ src/main/java/com/devcourse/dashbunny/domain/admin/Admin.java
@@ -0,0 +1,13 @@
+package com.devcourse.dashbunny.domain.admin;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Admin {
+
+    @Id
+    @GeneratedValue
+    private Long id; // 임시 코드입니다.
+}
