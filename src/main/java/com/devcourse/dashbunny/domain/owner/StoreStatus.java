0054fd527ef3b250fdf35d2a17ec615727f33dfb
Hojeong016 <skflsehdn@naver.com>
2024-11-16 15:33:15 +0900

    rename : StoreStatus 이넘 클래스 파일 옯김

---
+++ src/main/java/com/devcourse/dashbunny/domain/owner/StoreStatus.java
@@ -0,0 +1,12 @@
+package com.devcourse.dashbunny.domain.owner;
+
+public enum StoreStatus {
+        PENDING,    // 등록 대기 중
+        REGISTERED,  // 등록 완료
+        REGISTRATION, // 가게 등록 실패
+        OPEN, // 영업중
+        PENDING_OPEN, // 영업 준비 중
+        TEMP_CLOSE, // 휴업중
+        CLOSURE_PENDING, // 페업 신청 대기 중
+        CLOSED // 폐업
+}
