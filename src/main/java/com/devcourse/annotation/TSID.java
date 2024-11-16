04ec5dfc41dc01ba3617adc3279cbc673662aed9
Hojeong016 <skflsehdn@naver.com>
2024-11-16 15:34:36 +0900

    feat : TSID 생성 및 자동 할당 로직 추가
    - TSID 어노테이션 추가
    - TSIDListener 클래스 구현
    - UUID와 타임스탬프를 활용한 고유 식별자 생성

---
+++ src/main/java/com/devcourse/annotation/TSID.java
@@ -0,0 +1,12 @@
+package com.devcourse.annotation;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.FIELD)// 해당 어노테이션의 적용 범위 설정을 위한 어노테이션 (필드의 변수에만 지정가능하다.)
+@Retention(RetentionPolicy.RUNTIME) // 해당 어노테이션의 유지되는 시점 (런타임에 어노테이션 정보를 유지한다. )
+public @interface TSID{
+}
+
+//@Interface : 자바에서 새로운 어노테이션 타입을 적용할때 사용된다. 클래스처럼 정의 되며 해당 클래스에는 어노테이션의 행동에 대한 내용을 담지는 않는다.
+// 행동 -> 리스너 클래스(행동 정의 클래스)를 만들어 함께 사용한다.
+// 이유 -> 단일 책임의 원칙을 지키기 위해서라고 생각하면 된다.
---
+++ src/main/java/com/devcourse/lifecycle/TSIDListener.java
@@ -0,0 +1,42 @@
+package com.devcourse.lifecycle;
+
+
+import com.devcourse.annotation.TSID;
+import jakarta.persistence.PrePersist;
+import lombok.extern.slf4j.Slf4j;
+
+import java.lang.reflect.Field;
+import java.time.Instant;
+import java.util.UUID;
+
+@Slf4j
+public class TSIDListener {
+
+    @PrePersist
+    public void prePersist(Object entity) {
+
+        try {
+            // 엔티티 클래스의 모든 필드를 가져옴
+            Field[] fields = this.getClass().getDeclaredFields();
+            for (Field field : fields) {
+                //가져온 필드 중 TSID 어노테이션이 불으며 타입이 String인 경우를 필터링
+                if (field.isAnnotationPresent(TSID.class) && field.getType().equals(String.class)) {
+                    // 필드가 private 여도 접근을 허용할 수 있게한다.
+                    field.setAccessible(true);
+                    //TSID를 만들기
+                    String tsid = creatTSID();
+                    //TSID를 ID 필드에 할당
+                    field.set(entity,tsid);
+                }
+            }
+        }catch (IllegalAccessException e){
+            log.error("엔티티 아이디 주입 실패");
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String creatTSID() {
+        //현재 시간을 밀리초 단위의 타임스탬프로 반환.
+        return Instant.now().toEpochMilli() + UUID.randomUUID().toString();
+    }
+}
